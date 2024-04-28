cdbff335374b126363e515c4c8c85af9a7623f2c
stasjs <s.slesarev@crello.com>
2024-04-28 23:07:59 +0100

    add csvs

--- crypto-data/transform-hourly.ts
+++ crypto-data/transform-hourly.ts
@@ -1,6 +1,6 @@
 //@ts-ignore
 import fs from 'fs'; // File system access
-import dataImported from './btc-weekly.json'; // Sample JSON data
+import dataImported from './btc-4h.json'; // Sample JSON data
 
 // Sample JSON data
 const data: (string | number)[][] = dataImported;
@@ -15,17 +15,29 @@ function convertTimestamp(timestamp: number): string {
     return new Date(timestamp).toISOString().slice(0, 10); // YYYY-MM-DD
 }
 
+function convertTimestampToTime(timestamp: number): string {
+    // Divide by 1000 to convert milliseconds to seconds
+    const dateInSec = timestamp;
+    const time = new Date(dateInSec).toLocaleTimeString([], {
+      hour: '2-digit',
+      minute: '2-digit',
+      timeZone: "UTC",
+      hour12: false
+    });
+    return time.replace("24", "00"); // HH:MM format
+  }
+
 // Create CSV file
 const csvContent = data
     .map((item) => {
         const date = convertTimestamp(item[0] as number);
-        const row = [date, '00:00', item[1], item[2], item[3], item[4]];
+        const row = [date, convertTimestampToTime(item[0] as number), item[1], item[2], item[3], item[4]];
         return row.join(',');
     })
     .join('\n');
 
 fs.writeFileSync(
-    './crypto-data/btc-weekly.csv',
+    './crypto-data/btc-4h.csv',
     `${columns.join(',')}\n${csvContent}`
 );
 
